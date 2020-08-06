c2ad8fe09ad35038b5c052a1433a5ad990234c9e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-06 05:33:21 +0000

    build based on a0539cc

---
+++ v0.1-lw/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DPOMPs.jl","page":"DPOMPs.jl","title":"DPOMPs.jl","text":"","category":"section"},{"location":"","page":"DPOMPs.jl","title":"DPOMPs.jl","text":"***Fast Bayesian parameter inference for Discrete-state-space Partially Observed Markov Processes in Julia***","category":"page"}]
+}
