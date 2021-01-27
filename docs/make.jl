94b7a24d05ea8d32419c89963f438c8f3c239e58
mjb3 <martinjosephburke@gmail.com>
2021-01-27 22:38:38 +0000

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,8 @@ using Documenter, DPOMPs
 
 makedocs(sitename="DPOMPs.jl docs", pages = ["index.md", "models.md", "examples.md", "manual.md"])
 
+## nb. called by GitHub Actions wf
+# - local version deploys to build dir
 deploydocs(
     repo = "github.com/mjb3/DPOMPs.jl.git",
 )
